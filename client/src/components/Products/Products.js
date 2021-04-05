e14a22345b1fe2137d67cac033f654c641d69d37
Mavchi <aleksi.khlebnikov@gmail.com>
2021-04-05 11:32:17 +0300

    Basic functionality of <Shop>

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -5,46 +5,58 @@ import CardActionArea from '@material-ui/core/CardActionArea'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+
+import { Link } from 'react-router-dom'
 
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: "250px",
-    },
-    media: {
-        height: "250px",
-    },
+	root: {},
+	media: {
+		height: "250px",
+	},
 })
 
 const ProductCard = ({ product }) => {
-    const classes = useStyles()
-    console.log(product.thumbnail.default)
+	const classes = useStyles()
+	console.log(product.thumbnail.default)
 
-    return (
-        
-        <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                    className={classes.media}
-                    image={product.thumbnail.default}
-                    title={product.name}
-                />
-            </CardActionArea>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {product.name}
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+	return (
+		<Grid item xs={12} sm={6} md={4}>
+			<Card className={classes.root}>
+				<Link to={`/${product.uri}/`}>
+					<CardActionArea>
+						<CardMedia
+							className={classes.media}
+							image={product.thumbnail.default}
+							title={product.name}
+						/>
+					</CardActionArea>
+				</Link>
+				<CardContent>
+					<Typography align="center" variant="h5" component="h2">
+						{product.name}
+					</Typography>
+					<Typography align="center" variant="body1" component="h5">
+						{product.price + " €"}
+					</Typography>
+				</CardContent>
+			</Card>
+		</Grid>
+	)
 }
 
 const Products = ({ products }) => {
-    return (
-        <main>
-            {products.map(product => <ProductCard key={product.id} product={product} />)}
-        </main>
-    )
+	const classes = useStyles()
+
+	return (
+		<Container component="main" className={classes.cardGrid}>
+			<Grid container spacing={4}>
+				{products.map(product => <ProductCard key={product.id} product={product} />)}
+			</Grid>
+		</Container>
+	)
 }
 
 export default Products
